8ddce9b8c7d368bd89debc82a06c90740b12f299
ravi.kumar <sitravi.kumar@gmail.com>
2016-05-03 18:13:31 +0530

    Adding Java.util.concurrent Exercise

---
+++ Server/v1/Server.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server 
+{ 
+   public static void main(String[] args) throws IOException
+   {
+      ServerSocket socket = new ServerSocket(9000);
+      while (true) 
+      {
+         final Socket s = socket.accept();
+         Runnable r = new Runnable() 
+                      {
+                         @Override
+                         public void run() 
+                         {
+                            doWork(s);
+                         }
+                      };
+         new Thread(r).start();
+      }
+   }
+
+   static void doWork(Socket s)
+   {
+   }
+}
